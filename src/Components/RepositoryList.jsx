0ab6d936c935fc573d9d0af5492de55c1716cb16
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-11 20:59:10 -0300

    add: styles list

--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -1,7 +1,22 @@
+import { RepositoryItem } from './RepositoryItem'
+import "../styles/repositories.scss";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in react',
+  link: 'https://github.com/'
+}
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositorios</h1>
+      <ul> 
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+      </ul>
     </section>
   )
 }
